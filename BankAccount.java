1b0507b4bec410dde78fd3836da92e1080cd9666
Victor Fuentes <21144@columbushs.com>
2020-10-05 10:59:45 -0400

    home

--- BankAccount.java
+++ BankAccount.java
@@ -1,9 +1,20 @@
 public class BankAccount {
 
     // Instance Variables
-
+    private double balance;
     // Constructors
+    public BankAccount(){
+        balance = 0.0;
+    }
 
+    public BankAccount(double pBalance){
+        this.balance = pBalance;
+    }
     // Methods
-
+    public void setBalance(double pBalance){
+        this.balance = pBalance;
+    }
+    public double getBalance(){
+        return this.balance;
+    }
 }
